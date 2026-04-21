c50c184d6efc33938e9e76a9bee96b960ee92984
StanRemus <stanremus2004@yahoo.com>
2026-04-21 14:42:51 +0300

    Implementare RestClient

--- Cod/backend-ai/backend-ai/src/main/java/com/licenta/backend_ai/DiagnosticController.java
+++ Cod/backend-ai/backend-ai/src/main/java/com/licenta/backend_ai/DiagnosticController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.client.RestClient;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/ai-detection")
 @CrossOrigin(origins = "*")
@@ -18,5 +20,18 @@ public class DiagnosticController {
     private final String PYTHON_PATH = "http://127.0.0.1:8000/diagnostic/";
     private final RestClient restClient = RestClient.create();
 
+    @PostMapping("/analiza/")
+    public ResponseEntity<String> PozaLaAi(@RequestParam("file") MultipartFile file) throws IOException {
 
+        ByteArrayResource pozaPy = new ByteArrayResource(file.getBytes())
+        {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename() != null ? file.getOriginalFilename() : "poza_test.jpg";
+            }
+        };
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", pozaPy);
+        return null;
+    }
 }
